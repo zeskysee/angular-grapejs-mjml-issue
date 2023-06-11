1b20d1a775a78b4678333c991113d9dadbfb2472
See Zeying <zeskysee@gmail.com>
2023-06-12 00:54:08 +0800

    Update to use grapejs editor and mjml plugin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
+import grapesjs, {Editor} from 'grapesjs';
+import grapesJSMJML from 'grapesjs-mjml';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+  styleUrls: ['./app.component.less'],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'angular-grapejs-mjml-issue';
+
+  @ViewChild('gjs') gjs!: ElementRef<HTMLDivElement>;
+
+  public grapesjsEditor?: Editor;
+
+  ngAfterViewInit(): void {
+    try {
+      this.grapesjsEditor = grapesjs.init({
+        container: this.gjs.nativeElement,
+        fromElement: false,
+        storageManager: false,
+        height: 'calc(100vh - 108px)',
+        plugins: [grapesJSMJML],
+        assetManager: { assets: [] },
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  }
 }
